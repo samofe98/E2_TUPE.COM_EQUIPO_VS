6adcb179ed7bca46f3c83fd94d74d16fd3d16ec3
samofe98 <smontoyaf5@gmail.com>
2025-10-24 17:03:08 -0500

    update2410

--- src/infraestructure/config/mongoDB.ts
+++ src/infraestructure/config/mongoDB.ts
@@ -3,15 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Carga las variables del archivo .env
 
-/**
- * Se recomienda usar un archivo .env (ya manejado por dotenv)
- * Ejemplo:
- * MONGO_URI=mongodb+srv://usuario:password@cluster0.maprloz.mongodb.net/tuecommerce
- * DB_NAME=tuecommerce
- */
-
 const MONGO_URI: string = process.env.MONGO_URI || '';
-const DB_NAME: string = process.env.DB_NAME || 'tuecommerce';
 
 export const dbConnection = async (): Promise<void> => {
   try {
@@ -20,11 +12,10 @@ export const dbConnection = async (): Promise<void> => {
     }
 
     await mongoose.connect(MONGO_URI, {
-      dbName: DB_NAME,
       serverSelectionTimeoutMS: 5000,
     });
 
-    console.log(`✅ [DB] Conectado exitosamente a MongoDB Atlas - Base: ${DB_NAME}`);
+    console.log(`✅ [DB] Conectado exitosamente a MongoDB Atlas`);
   } catch (error) {
     console.error('❌ [DB] Error al conectar con MongoDB:', error);
     process.exit(1);
